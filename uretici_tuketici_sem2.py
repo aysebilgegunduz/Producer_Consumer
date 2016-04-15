424cd50926ee82d92cb6a7a2a2904cf3939c8473
Ayşe Bilge İnce <aysebilgegunduz@gmail.com>
2016-04-15 19:29:04 +0300

    final version

--- uretici_tuketici_sem2.py
+++ uretici_tuketici_sem2.py
@@ -1,42 +1,50 @@
 __author__ = 'bilge'
 from threading import Thread, Semaphore, Lock
+import time
+import random
 mutex = Lock()
-
-class uretici_tuketici():
-    def __init__(self,n):
-        self.buf = []
-        self.out_ = 0
-        self.in_ = 0
-        self.bos = Semaphore(value=n)
-        self.dolu = Semaphore(value=0)
-
-    def Producer(self, n, veri):
-        for i in (0,n):
-            item = i
-            self.bos.acquire()
-            mutex.acquire()
-            self.buf.append(item)
-            self.in_ = (self.in_ + 1) % n
-            print("P["+str(i)+"] Producing "+ str(item))
-            mutex.release()
-            self.dolu.release()
-    def Consumer(self,n):
-        for i in (0,n):
-            self.dolu.acquire()
-            mutex.acquire()
-            item = self.buf[self.out_]
-            self.out_ = (self.out_ + 1) % n
-            print("C["+str(i)+"] Consuming "+str(item))
-            mutex.release()
-            self.bos.release()
-
-idP = Thread()
-idC = Thread()
 index = 0
-prod_cons = uretici_tuketici(10)
-for i in (0,100):
-    idP = Thread(target=prod_cons.Producer(3,i), args=4)
-    idP.start()
-    idC = Thread(target=prod_cons.Consumer(3), args=4)
-    idC.start()
+n=3
+buf = []
+out_ = 0
+in_ = 0
+bos = Semaphore(value=n)
+dolu = Semaphore(value=0)
+class Producer(Thread):
+
+    def run(self):
+        global buf
+        global in_
+        global bos
+        global dolu
+        while True:
+            nums = range(5)
+            item = random.choice(nums)
+            bos.acquire()
+            mutex.acquire()
+            buf.append(item)
+            in_ = (in_ + 1) % n
+            print("P["+str(in_)+"] Producing "+ str(item))
+            mutex.release()
+            dolu.release()
+            time.sleep(random.random())
+class Consumer(Thread):
+    def run(self):
+        global buf
+        global out_
+        global bos
+        global dolu
+        while True:
+            dolu.acquire()
+            mutex.acquire()
+            item = buf[out_]
+            out_ = (out_ + 1) % n
+            print("C["+str(out_)+"] Consuming "+str(item))
+            mutex.release()
+            bos.release()
+            time.sleep(random.random())
+
+
+Producer().start()
+Consumer().start()
 
